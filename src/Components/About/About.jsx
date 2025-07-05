67662dfff26c4b83f0633658c6eaafabe9742741
nalina <naleenameshram85@gmail.com>
2025-07-05 22:49:36 +0530

    done

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           {/* Resume Button */}
 
           <a
-            href="https://drive.google.com/file/d/1kDoPeCXR1ba6zDfUshgILYr_GjiXTsta/view?usp=sharing"
+            href="https://drive.google.com/file/d/13mixRPEBNDbg2JhzPWkzd3pLKYJCX269/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
